25019a3193dd0cce791e2d1b372c1211d34235b0
Princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-20 11:31:32 +0530

    for-of Loop.js
    /*  for-of loop
    loops are used to execute a piece of code again & again
    */

---
+++ for-of Loop.js
@@ -0,0 +1,11 @@
+/*  for-of loop
+loops are used to execute a piece of code again & again
+*/
+
+let str = "PrincePanchal";
+let size = 0;
+for(let i of str){ // Iteration -> characters
+  console.log("i =",i);
+  size++;
+}
+  console.log("String size is =",size); // 13
